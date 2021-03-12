6c517b276332515e7bf39ea62d0ee3e46dfb6224
ptorngh <patrik.tornberg@hm.com>
2021-03-12 15:48:00 +0100

    first commit

---
+++ src/main/java/com/example/Ecosmartsystem/EcosmartsystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Ecosmartsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcosmartsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcosmartsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Ecosmartsystem/EcosmartsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Ecosmartsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcosmartsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
